529c9383a788aa54491dadbe157d0287c90e480c
Emanuel Palm <emanuelpalm@gmail.com>
2020-03-23 10:01:48 +0100

    Initial commit

---
+++ echo/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+trap 'pkill -P $$' SIGINT SIGTERM
+
+mvn package
+
+CP="$(find . -wholename './target/*-jar-with-dependencies.jar' | tr '\n' ':')"
+
+java -cp "${CP}" se.arkalix.examples.EchoSystem \
+  ./crypto/system-keystore.p12 ./crypto/system-truststore.p12 &
+
+sleep 5s
+
+java -cp "${CP}" se.arkalix.examples.EchoClient \
+  ./crypto/client-keystore.p12 ./crypto/client-truststore.p12 &
+
+wait %1 %2
+
+echo "Done!"
